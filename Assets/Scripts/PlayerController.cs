5c6b2943b20784d40ed1004a240d34deb98adb34
Audric Chabert <audric.chabert.r@gmail.com>
2022-09-22 22:21:00 +0200

    working & efficient boundaries check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,15 @@ public class PlayerController : MonoBehaviour
     Vector3 inputs;
     //ref https://www.youtube.com/watch?v=wlZUu-I05Bk
 
+
+    float terrainBoundaryMinXFloat = 20;
+    float terrainBoundaryMaxXFloat = 230;
+    float terrainBoundaryMinYFloat = 0;
+    float terrainBoundaryMaxYFloat = 100;
+    float terrainBoundaryMinZFloat = 20;
+    float terrainBoundaryMaxZFloat = 230;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +27,7 @@ public class PlayerController : MonoBehaviour
     {
         GetInputs();
         AddForce();
+        CheckBoundaries();
     }
 
     void GetInputs()
@@ -43,5 +53,17 @@ public class PlayerController : MonoBehaviour
     void AddForce()
     {
         GetComponent<Rigidbody>().AddRelativeForce(new Vector3(inputs.y, inputs.z, inputs.x));
+    }
+
+    void CheckBoundaries()
+    {
+        float xPosition = Mathf.Clamp(transform.position.x, terrainBoundaryMinXFloat, terrainBoundaryMaxXFloat);
+        float yPosition = Mathf.Clamp(transform.position.y, terrainBoundaryMinYFloat, terrainBoundaryMaxYFloat);
+        float zPosition = Mathf.Clamp(transform.position.z, terrainBoundaryMinZFloat, terrainBoundaryMaxZFloat);
+
+        transform.position = new Vector3(xPosition, yPosition, zPosition);
+
+        //Todo : remove the relativeForce when colliding with the boundaries, so that the player doesn't get "stuck" to the boundary because the force continues to push him against it
+        //Todo : add a vector arrow to display the current force, use the tutorial video with the angle of attack (probably a playerController video)
     }
 }
